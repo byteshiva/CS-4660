33bb0f68e317837709ee9413c4af84b30d91d27c
Russ Abbott <Russ.Abbott@gmail.com>
2019-10-02 09:27:35 -0700

    Latest version

--- 2019/Kopek_Constraint_Satisfaction/csp.py
+++ 2019/Kopek_Constraint_Satisfaction/csp.py
@@ -2,7 +2,7 @@
 # From Classic Computer Science Problems in Python Chapter 3
 # Copyright 2018 David Kopec
 #
-# Modified by Russ Abbott (2019)
+# With relatively minor modifications by Russ Abbott (7/2019)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,78 +16,113 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Generic, TypeVar, Dict, List, Optional
-from abc import ABC, abstractmethod
-
-V = TypeVar('V')  # variable type
-D = TypeVar('D')  # domain type
+# Modified by Russ Abbott (2019)
 
 
-# Base class for all constraints
-class Constraint(Generic[V, D], ABC):
-    # The variables that the constraint is between
-    def __init__(self, variables: List[V]) -> None:
-        self.variables = variables
+# The first import allows the use of CSP as a type in all_different, a method within CSP.
+# It shouldn't be necessary in Python 3.8
+from __future__ import annotations
 
-    # Must be overridden by subclasses
-    @abstractmethod
-    def satisfied(self, assignment: Dict[V, D]) -> bool:
-        ...
+from typing import Callable, Dict, Generator, Generic, List, Optional, Set, TypeVar
+
+V = TypeVar('V')  # the type of the variables
+D = TypeVar('D')  # the type of the domain elements
 
 
-# A constraint satisfaction problem consists of variables of type V
-# that have ranges of values known as domains of type D and constraints
-# that determine whether a particular variable's domain selection is valid
 class CSP(Generic[V, D]):
-    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
-        self.variables: List[V] = variables  # variables to be constrained
-        self.domains: Dict[V, List[D]] = domains  # domain of each variable
-        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
-        self.low_mark = len(variables)
-        self.count = 0
-        for variable in self.variables:
-            self.constraints[variable] = []
-            if variable not in self.domains:
-                raise LookupError("Every variable should have a domain assigned to it.")
+    """
+    A constraint satisfaction problem consists of 
+        a) variables of type V that have 
+        b) domains of values of type D and 
+        c) constraints that determine the validity of a given assignment of values to variables.  
+    """
+    
+    def __init__(self, variables: List[V], domains: Dict[V, Set[D]], assignment_limit=0) -> None:
+        self.variables: List[V] = variables  # the variables to be constrained
+        self.domains: Dict[V, Set[D]] = domains  # the domain of each variable
+        self.constraints: List[Callable[..., bool]] = []
+        
+        # Keeps track of the number of assignments created
+        self.assignment_limit = assignment_limit
+        self.assignments = 0
+        self.printed_reached_limit = False
 
-    def add_constraint(self, constraint: Constraint[V, D]) -> None:
-        for variable in constraint.variables:
-            if variable not in self.variables:
-                raise LookupError("Variable in constraint not in CSP")
-            else:
-                self.constraints[variable].append(constraint)
+    def add_constraint(self, constraint: Callable[..., bool]) -> None:
+        self.constraints.append(constraint)
 
-    # Check if the value assignment is consistent by checking all constraints
-    # for the given variable against it
-    def consistent(self, variable: V, assignment: Dict[V, D]) -> bool:
-        for constraint in self.constraints[variable]:
-            if not constraint.satisfied(assignment):
-                return False
-        return True
+    @staticmethod
+    def all_different(_csp: CSP, _newly_assigned_variable, assignment: Dict[V, D]):
+        """
+        Are all values in the assignment unique?
+        
+        This is such a common constraint that we are including it here.
+        """
+        return len(set(assignment.values())) == len(assignment)
+
+    def complete_the_assignment(self, assignment: Dict[V, D]) -> Optional[Dict[V, D]]:
+        """
+        Add variable/value pairs to assignment until it either fails a consistsency check if satisfies the constraints.
+        :param assignment:
+        :return: a consistent assignment or None if no extention assignment satisfies the constraints.
+        """
+
+        # Make a generator that returns all variables that have not yet been assigned values.
+        # A generator is like a list comprehension but one that returns values as needed.
+        # It's a form of lazy evaluation.
+        unassigned_variables: Generator[V] = (v for v in self.variables if v not in assignment)
+        # We use only the first variable. There was no need to generate all of them.
+        selected_variable: V = next(unassigned_variables)  
+
+        # Try all possible domain values of the selected variable
+        for selected_value in self.domains[selected_variable]:
 
 
-    # noinspection PyDefaultArgument
-    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
-        # assignment is complete if every variable is assigned (our base case)
-        if len(assignment) == len(self.variables):
-            return assignment
+            # Extend the current assignment with: selected_variable: selected_value.
+            # **assignment lists the elements of assignment.
+            # So the following line creates a new dictiony with all the elements of
+            # assignment plus the assignment to selected_variable.  selected_variable: selected_value
+            extended_assignment = {**assignment, selected_variable: selected_value}
+            self.assignments += 1
 
-        # get all variables in the CSP but not in the assignment
-        unassigned: List[V] = [v for v in self.variables if v not in assignment]
-        nbr_left = len(unassigned)
-        if nbr_left <= self.low_mark:
-            self.count += 1
-            print(nbr_left, end='\n' if self.count % 20 == 0 else ' ')
-            self.low_mark = nbr_left
-        # get the possible domain values of the first unassigned variable
-        first: V = unassigned[0]
-        for value in self.domains[first]:
-            local_assignment = assignment.copy()
-            local_assignment[first] = value
-            # if we're still consistent, we recurse (continue)
-            if self.consistent(first, local_assignment):
-                result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
-                # if we didn't find the result, we will end up backtracking
-                if result is not None:
-                    return result
+            if self.assignment_limit:
+                if self.assignments <= self.assignment_limit:
+                    print((f'{self.assignments:2}' if self.assignments < 100 else f'{self.assignments}') +
+                          f'. {extended_assignment}')
+                else:
+                    if not self.printed_reached_limit:
+                        print(f'Reached the assignment_limit of {self.assignment_limit}. ')
+                        self.printed_reached_limit = True
+                    self.assignments = self.assignment_limit
+                    return None
+
+            if not self.is_consistent(selected_variable, extended_assignment):
+                continue
+
+            # The assignment is consistent with the constraints. Is it complete?
+            # An assignment is complete if every variable is assigned a value.
+            # The next line is True only when we have just added the final variable/value combination to assignment.
+            if len(extended_assignment) == len(self.variables):
+                return extended_assignment
+
+            # The assignment is consistent but not complete.
+            # Use recursion to complete the assignment.
+            # result will be either a valid assignment or None if no valid completion is found.
+            result = self.complete_the_assignment(extended_assignment)
+
+            # result is None if we didn't find a valid assignment for the remaining variables.
+            # In that case, try the next value for selected_variable.
+            if result is None:
+                continue
+
+            # result is a valid assignment. Return it.
+            return result
+
+        # If no value for selected_variable yields a valid assignment, return None and
+        # let the previous recursive level try the next value for its variable.
         return None
+
+    def is_consistent(self, newly_assigned_variable, assignment: Dict[V, D]) -> bool:
+        """
+        Is assignment consistent with all constraints?
+        """
+        return all(constraint(self, newly_assigned_variable, assignment) for constraint in self.constraints)
--- 2019/Kopek_Constraint_Satisfaction/send_more_money.py
+++ 2019/Kopek_Constraint_Satisfaction/send_more_money.py
@@ -13,47 +13,78 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from csp import Constraint, CSP
-from typing import Dict, List, Optional
+
+# Modified by Russ Abbott (2019)
+
+from typing import Dict, List
+
+from csp import CSP
 
 
-class SendMoreMoneyConstraint(Constraint[str, int]):
-    def __init__(self, letters: List[str]) -> None:
-        super().__init__(letters)
-        self.letters: List[str] = letters
+def digits_to_number(digits: List[int]) -> int:
+    """
+    Convert a sequence of digits into a number.
+    E.g., digits_to_number([1, 2, 3]) -> 123
+    """
+    return int(''.join(map(str, digits)))
 
-    def satisfied(self, assignment: Dict[str, int]) -> bool:
-        # if there are duplicate values then it's not a solution
-        if len(set(assignment.values())) < len(assignment):
-            return False
 
-        # if all variables have been assigned, check if it adds correctly
-        if len(assignment) == len(self.letters):
-            s: int = assignment["S"]
-            e: int = assignment["E"]
-            n: int = assignment["N"]
-            d: int = assignment["D"]
-            m: int = assignment["M"]
-            o: int = assignment["O"]
-            r: int = assignment["R"]
-            y: int = assignment["Y"]
-            send: int = s * 1000 + e * 100 + n * 10 + d
-            more: int = m * 1000 + o * 100 + r * 10 + e
-            money: int = m * 10000 + o * 1000 + n * 100 + e * 10 + y
-            return send + more == money
-        return True # no conflict
+def send_more_money_constraint(_csp: CSP, _newly_assigned_variable, assignment: Dict[str, int]) -> bool:
+    """
+
+    :param _csp: not used
+    :param _newly_assigned_variable:  Not used
+    :param assignment: the current assignment
+    :return: True/False: Is the current assignment consistent with the constraint?
+             Consistent doesn't mean the problem is solved, only that
+             the current assignment is not inconsistent with the constraint.
+             (The current assignment may be consistent but incomplete.)
+    """
+    if set('SENDMORY') == set(assignment.keys()):
+        # If all variables are assigned, do the numbers add  up?
+        (s, e, n, d, m, o, r, y) = (assignment[x] for x in 'SENDMORY')
+        return (digits_to_number([s, e, n, d]) + digits_to_number([m, o, r, e])) == digits_to_number([m, o, n, e, y])
+    else:
+        # Not all variables have been assigned. We are not checking any other form of consistency.
+        # (all_different is checked by the all_different constraint.)
+        # In particular, we are not checking to see, for example, whether the first column adds up.
+        return True
+
+
+def display_result(term_1, term_2, sum, solution, assignments):
+    if solution is None:
+        print(f"\nNo solution found after {assignments} assignments!")
+        return
+
+    print(f"\nSolution found after {assignments} assignments!")
+    print(f'{solution}\n')
+
+    term_1_solution = ''.join([str(solution[x]) for x in term_1])
+    term_2_solution = ''.join([str(solution[x]) for x in term_2])
+    sum_solution = ''.join([str(solution[x]) for x in sum])
+    print(f'  {term_1} ->  {term_1_solution}')
+    print(f'+ {term_2} ->  {term_2_solution}')
+    print(f'{"-" * (len(sum) + 1)}    {"-" * (len(sum))}')
+    print(f' {sum} -> {sum_solution}')
 
 
 if __name__ == "__main__":
-    letters: List[str] = ["S", "E", "N", "D", "M", "O", "R", "Y"]
-    possible_digits: Dict[str, List[int]] = {}
-    for letter in letters:
-        possible_digits[letter] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    possible_digits["M"] = [1]  # so we don't get answers starting with a 0
-    csp: CSP[str, int] = CSP(letters, possible_digits)
-    csp.add_constraint(SendMoreMoneyConstraint(letters))
-    solution: Optional[Dict[str, int]] = csp.backtracking_search()
-    if solution is None:
-        print("No solution found!")
-    else:
-        print(solution)
+    variables = 'SENDMORY'
+    digits = set(range(10))
+    domains = {var: digits for var in variables}
+    domains['S'] = domains['S'] - {0}  # What if we wrote: domains['S'] = {8, 9}
+
+    # Set assignment_limit to 0 to turn off tracing. A positive number indicates
+    # the number of assignments to allow (and to display) before quitting.
+    assignment_limit = 0
+
+    # Create the CSP object
+    csp: CSP[str, int] = CSP(list(variables), domains, assignment_limit)
+    csp.add_constraint(csp.all_different)
+    csp.add_constraint(send_more_money_constraint)
+
+    # The argument is an assignment used to start the process of assigning values to variables.
+    # SInce 'M' must be 1, plug that into the initial assignment.
+    solution = csp.complete_the_assignment({'M': 1})
+    assignments = csp.assignments
+    display_result('SEND', 'MORE', 'MONEY', solution, assignments)
